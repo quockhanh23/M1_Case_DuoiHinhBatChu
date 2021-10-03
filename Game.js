b41dab8dcde9401e83804c222e677783ac37366e
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-10-03 16:52:16 +0700

    flashlight

--- Game.js
+++ Game.js
@@ -1,63 +1,119 @@
-class Game {
-    music;
-    lisQuestion;
-
-    constructor(music, lisQuestion) {
-        this.music = music;
-        this.lisQuestion = lisQuestion;
-    }
-
-    showScreen() {
-        return this.lisQuestion;
-
-    }
-
-    ShowQuestion() {
+let name = prompt("Nhập tên")
+document.getElementById('ten').innerHTML = "Tên: " + name
+let arrQuestion = ["anh1.jpg", "anh2.jpg", "anh3.jpg", "anh4.jpg", "anh5.jpg", "anh6.jpg"];
+let arrAns = ["sóc trăng", "yếu ớt", "điếu cầy", "chó", "mắt lác", "óc chó"];
+let arrAns2 = ['Con sóc và mặt trăng có liên quan đến nhau không?', '2 quả ớt trông thế nào?',
+    'hút hít phê pha', 'con gì sủa gâu gâu', 'mắt anh làm sao thế?', 'tên 1 loại quả (hạt) ,' +
+    ' hay được dùng để chửi người khác :v']
+let point = 0
+let index = 1
+let time = 30;
 
 
-    }
+function check() {
 
-    checkAnswer() {
+    let answer = document.getElementById("nhập").value;
+    let question = document.getElementById("anh1");
+    let srcSplit = question.src.split("/");
+    let src = srcSplit[srcSplit.length - 1];
+    let index = arrQuestion.indexOf(src);
+    if (answer == arrAns[index]) {
+        alert("đúng")
+        document.getElementById('audio1').play();  // dẫn id file nhạc vào đây
+        question.src = arrQuestion[index + 1];
+        point = point + 2
 
-    }
+        document.getElementById("res2").innerHTML = "Điểm của bạn: " + point
+    } else
+        // lỗi audio
+        alert("sai")
 
-    addScore() {
+    point--
+    document.getElementById("res2").innerHTML = "Điểm của bạn: " + point
 
-    }
+    if (index == 5) {
 
-    moveNextQuestion() {
-
-
-    }
-
-    showHint() {
-
-    }
-
-    showScore() {
-
-    }
-
-    showTimeDuration() {
-
-    }
-
-    showSaveNameDialog() {
-
-    }
-
-    playBackgroundMusic() {
-        return this.music
-
-    }
-
-    showCharts() {
+        document.write("<h3 style='text-align: center;'>Bạn đã thắng" +
+            "<h3  style='text-align: center'>" + "Người chơi: " + name +
+            " ,Điểm của bạn " + point + "<br>" +
+            "<br>" + "Trò chơi kết thúc" + "<br>" +
+            "<br>" + "Bạn có muốn chơi lại không??" + "<br>" +
+            '<button style="font-size: 30px; ' +
+            'color: #008CBA;' +
+            'border-radius: 15px;' +
+            'border: none; ' +
+            'background-color: #008CBA;' +
+            'text-decoration: white' +
+            ' "><a href="index.html">Chơi lại</a></button>' +
+            '<button style="font-size: 30px; color: #008CBA; border-radius: 15px;' +
+            'background-color: #008CBA; border: none;" ' +
+            '><a href="index3.html">Không chơi nữa đâu!</a></button>')
 
     }
+    document.getElementById('res').innerHTML = "Câu trả lời của bạn là: " + answer
+    time = 30
+}
 
-    endGame() {
+countdown()
 
+function audio() {
+    document.getElementById('audio').play()
+}
+
+function tips() {
+    let question = document.getElementById("anh1");
+    let srcSplit = question.src.split("/");
+    let src = srcSplit[srcSplit.length - 1];
+    let index = arrQuestion.indexOf(src);
+    document.getElementById('res').innerHTML =
+        "<h3 style='text-align: right'>"
+        + arrAns2[index] + "</h3>"
+}
+
+function changeImage() {
+    time = 30
+
+    document.getElementById("anh1").src = arrQuestion[index]
+    index++
+    point--
+    document.getElementById("res2").innerHTML = "Điểm của bạn: " + point
+    if (index == 7) {
+
+        alert("hết rồi")
+
+        document.write("<h3 style='text-align: center;'>Bỏ qua nhiều quá" +
+            "<h3  style='text-align: center'>" + "Người chơi: " + name +
+            " ,Điểm của bạn " + point + "<br>" +
+            "<br>" + "Trò chơi kết thúc" + "<br>" +
+            "<br>" + "Bạn có muốn chơi lại không??" + "<br>" +
+            '<button style="font-size: 30px; ' +
+            'color: #008CBA;' +
+            'border-radius: 15px;' +
+            'border: none; ' +
+            'background-color: #008CBA;' +
+            'text-decoration: white' +
+            ' "><a href="index.html">Chơi lại</a></button>' +
+            '<button style="font-size: 30px; color: #008CBA; border-radius: 15px;' +
+            'background-color: #008CBA; border: none;" ' +
+            '><a href="index3.html">Không chơi nữa đâu!</a></button>')
     }
 
+}
 
+function countdown() {
+    if (time === 0) {
+        document.getElementById('audio2').play()
+        alert('hết giờ')
+
+    } else {
+        document.getElementById('time').innerHTML = time
+        time--
+        setTimeout(countdown, 1000)
+    }
+}
+function myFunction() {
+    document.getElementById('nhập').style.background = "yellow";
+}
+function myFunction2() {
+    document.getElementById('nhập').style.background = "#9fd7ea";
 }
---
+++ credit.js
@@ -0,0 +1,35 @@
+function oki() {
+    document.getElementById('check1').disabled = true
+    document.getElementById('check2').disabled = true
+    document.getElementById('check3').disabled = true
+    document.getElementById("rss").innerHTML = "Cảm ơn và hẹn gặp lại !!"
+    document.getElementById('audio1i3').play()
+
+}
+function myFunction() {
+    document.getElementById('nhập1').style.background = "#627705";
+    document.getElementById('nhập2').style.background = "#549f58";
+    document.getElementById('nhập3').style.background = "green";
+    document.getElementById('nhập4').style.background = "yellow";
+    document.getElementById('nhập5').style.background = "indigo";
+    document.getElementById('nhập11').style.background = "blue";
+    document.getElementById('nhập22').style.background = "#d55313";
+    document.getElementById('nhập33').style.background = "#110825";
+    document.getElementById('nhập44').style.background = "yellow";
+    document.getElementById('nhập55').style.background = "#a016b6";
+
+}
+
+function myFunction2() {
+    document.getElementById('nhập1').style.background = "#14197e";
+    document.getElementById('nhập2').style.background = "#b010dc";
+    document.getElementById('nhập3').style.background = "#b20e0e";
+    document.getElementById('nhập4').style.background = "#9fd7ea";
+    document.getElementById('nhập5').style.background = "#98b073";
+    document.getElementById('nhập11').style.background = "#12f574";
+    document.getElementById('nhập22').style.background = "#5d0404";
+    document.getElementById('nhập33').style.background = "#b20e0e";
+    document.getElementById('nhập44').style.background = "#dcc531";
+    document.getElementById('nhập55').style.background = "#181717";
+
+}
